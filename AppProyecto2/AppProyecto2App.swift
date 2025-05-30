3b60d58b237069720f04d024406139bce66138d1
MiriamASanchezC <miriamaliciasanchezcervantes@gmail.com>
2025-05-29 23:32:13 -0600

    Initial Commit

---
+++ AppProyecto2/AppProyecto2App.swift
@@ -0,0 +1,17 @@
+//
+//  AppProyecto2App.swift
+//  AppProyecto2
+//
+//  Created by Miriam Sanchez on 29/05/25.
+//
+
+import SwiftUI
+
+@main
+struct AppProyecto2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppProyecto2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppProyecto2
+//
+//  Created by Miriam Sanchez on 29/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
